7a6ab8a2eae9b701354b31ac57bdbf24ac09aee0
Ayush Gaikwad <ayushgaikwad0605@gmail.com>
2025-11-25 19:49:58 +0530

    login bug fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,109 +1,102 @@
 const createHttpError = require("http-errors");
-const User=require("./../models/userModel");
-const bcrypt=require('bcrypt');
-const jwt=require('jsonwebtoken');
-const config= require('./../config/config.js')
+const User = require("./../models/userModel");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('./../config/config.js')
 
-const register=async (req,res,next)=>{
+const register = async (req, res, next) => {
     try {
-                const {name,phone,email,password,role}=req.body; 
-                if(!name ||!phone ||!email ||!password ||!role)
-                {
-                    const error=createHttpError(400,"All Fields Are Required!")
-                    next(error); 
-                }
+        const { name, phone, email, password, role } = req.body;
+        if (!name || !phone || !email || !password || !role) {
+            const error = createHttpError(400, "All Fields Are Required!")
+            next(error);
+        }
 
-                const isUserPresent=await User.findOne({email});
-                if(isUserPresent)
-                {
-                    const error=createHttpError(400,"User Already Exists!");
-                    next(error);
-                }
+        const isUserPresent = await User.findOne({ email });
+        if (isUserPresent) {
+            const error = createHttpError(400, "User Already Exists!");
+            next(error);
+        }
 
-                const user={name,email,phone,password,role}; 
-                const newuser=User(user); 
-                 await newuser.save();
+        const user = { name, email, phone, password, role };
+        const newuser = User(user);
+        await newuser.save();
 
-                res.status(201).json({success:true,message:"New User Created!",data:newuser})
+        res.status(201).json({ success: true, message: "New User Created!", data: newuser })
 
 
     }
-    catch (error)
-    {
+    catch (error) {
         next(error)
     }
 
 }
 
 
-const login=async (req,res,next)=>{
+const login = async (req, res, next) => {
 
     try {
-        const {email,password}=req.body; 
-    
-        if(!email || !password)
-        {
-            const error=createHttpError(400,"All Fields Are Required !")
-               return next(error); 
-            
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            const error = createHttpError(400, "All Fields Are Required !")
+            return next(error);
+
         }
 
-       const isUserPresent=await User.findOne({email});
+        const isUserPresent = await User.findOne({ email });
 
-       if(!isUserPresent)
-       {
-            const error=createHttpError(401,"Invalid Credentials");
-           return next(error);
-       }
+        if (!isUserPresent) {
+            const error = createHttpError(401, "Invalid Credentials");
+            return next(error);
+        }
 
-       const isMatch=await bcrypt.compare(password,isUserPresent.password); 
-       if(!isMatch)
-       {
-        const error=createHttpError(401,"Invalid Credentials");
-            return next(error); 
-       }
+        const isMatch = await bcrypt.compare(password, isUserPresent.password);
+        if (!isMatch) {
+            const error = createHttpError(401, "Invalid Credentials");
+            return next(error);
+        }
 
-       const accessToken=jwt.sign({_id:isUserPresent._id},config.accessTokenSecret,{ 
-        expiresIn:'1d'
-       })
+        const accessToken = jwt.sign({ _id: isUserPresent._id }, config.accessTokenSecret, {
+            expiresIn: '1d'
+        })
 
-      res.cookie('accessToken', accessToken, {
-  httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  maxAge: 1000 * 60 * 60 * 24 * 30,
-  path: "/" // VERY IMPORTANT!!!
-});
+        res.cookie('accessToken', accessToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 1000 * 60 * 60 * 24 * 30,
+            path: "/" // VERY IMPORTANT!!!
+        });
 
 
-       res.status(200).json({success:true,message:"User Login Successfully!",data:isUserPresent})
-    
+        res.status(200).json({ success: true, message: "User Login Successfully!", data: isUserPresent })
+
     } catch (error) {
         next(error);
     }
 }
 
-const getUserData=async(req,res,next)=>{
-    
-   try {
-     const user=await User.findById(req.user._id); 
-     res.status(200).json({success:true,data:user})
-   } catch (error) {
-       return next(error);
-   }
-}
+const getUserData = async (req, res, next) => {
 
-
-const logout=async(req,res,next)=>{
     try {
-            res.clearCookie('accessToken');
-            res.status(200).json({sucess:true,message:"User logout successfully"}); 
-    }
-
-    catch (error)
-    {
-        next(error); 
+        const user = await User.findById(req.user._id);
+        res.status(200).json({ success: true, data: user })
+    } catch (error) {
+        return next(error);
     }
 }
 
-module.exports={register,login,getUserData,logout};
+
+const logout = async (req, res, next) => {
+    try {
+        res.clearCookie('accessToken');
+        res.status(200).json({ sucess: true, message: "User logout successfully" });
+    }
+
+    catch (error) {
+        next(error);
+    }
+}
+
+module.exports = { register, login, getUserData, logout };
